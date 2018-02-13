43bdff9e025d68cf2551e8f9a07765736eee881c
Michael Olie <aolie94@gmail.com>
2018-02-12 16:04:12 -0800

    changed root page, added link to store, and generated cart scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  resources :carts
+  root to: 'pages#index'
+  get 'visitors', to: 'visitors#index' 
   devise_for :users
   resources :users
   resources :products
