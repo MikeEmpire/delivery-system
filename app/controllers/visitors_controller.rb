c882cc8750d9d67cc26dc4a4fcdf71e6da2f9427
Michael Olie <aolie94@gmail.com>
2018-02-13 07:40:55 -0800

    created cart and line items, and link to each item on the front page

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,4 +2,6 @@ class VisitorsController < ApplicationController
   def index
     @products = Product.all
   end
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'store/index'
+
+  resources :line_items
   resources :carts
   root to: 'pages#index'
-  get 'visitors', to: 'visitors#index' 
+  get 'visitors', to: 'visitors#index'
   devise_for :users
   resources :users
   resources :products
