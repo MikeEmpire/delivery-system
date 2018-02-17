e7666d7e53251c598dbdeff4892934cd8e68d561
Michael Olie <aolie94@gmail.com>
2018-02-16 16:44:45 -0800

    added order functionality to project

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :line_items
+  has_many :orders, through: :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
   has_attached_file :image, style: { large: "600x600>", medium: "300x300>", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
