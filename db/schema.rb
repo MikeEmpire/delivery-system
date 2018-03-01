b9f340602256545e2f286f493695322a3fcd318d
Michael Olie <aolie94@gmail.com>
2018-02-28 16:05:38 -0800

    pay options for product to put in cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180223153305) do
+ActiveRecord::Schema.define(version: 20180226095240) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20180223153305) do
     t.integer "image_file_size"
     t.datetime "image_updated_at"
     t.string "category"
+    t.decimal "price"
   end
 
   create_table "users", force: :cascade do |t|
