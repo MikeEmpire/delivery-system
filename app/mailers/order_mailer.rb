503a6959a4ea4e5eccae2f971223deb4b505b471
Michael Olie <aolie94@gmail.com>
2018-03-02 18:19:02 -0800

    email confirmation on orders using ActionMailer

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,24 @@
+class OrderMailer < ApplicationMailer
+  default from: '1Up Delivery <1updelivered@gmail.com>'
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.received.subject
+  #
+  def received(order)
+    @order = order
+    
+    mail to: @order.email, subject: '1Up Delivery service order confirmation'
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.shipped.subject
+  #
+  def shipped
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
