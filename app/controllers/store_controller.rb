9c40ba64202428a3c3a9b9ae63017e77f83b6ece
Michael Olie <aolie94@gmail.com>
2018-03-05 09:59:19 -0800

    modified controller to show products by category

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,7 @@ class StoreController < ApplicationController
 
   def index
     @products = Product.order(:category)
+    @exclusive = Product.where(category: "Exclusive")
+    @PR = Product.where(category: "PR")
   end
 end
